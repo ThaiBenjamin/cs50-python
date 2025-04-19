507dfbf8c7b5b0c183e067816173c1b0a96dacb9
ThaiBenjamin <132616645+ThaiBenjamin@users.noreply.github.com>
2025-04-19 06:02:18 +0000

    Fri, Apr 18, 2025, 11:02 PM -07:00

---
+++ faces/faces.py
@@ -0,0 +1,13 @@
+def convert(face):
+    if face = ":)":
+        return 🙂
+    elif face = ":(":
+        return 🙁
+    else:
+        return
+
+user_input = input("")
+new_emoji = convert(user_input)
+print(new_emoji)
+
+
