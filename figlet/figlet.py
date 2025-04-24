f6a5b1c3a4e5814f7caad84127061ea8ca56dcc6
ThaiBenjamin <132616645+ThaiBenjamin@users.noreply.github.com>
2025-04-24 02:40:08 +0000

    Wed, Apr 23, 2025, 7:40 PM -07:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -2,7 +2,13 @@ from pyfiglet import Figlet
 import sys, random
 
 figlet = Figlet()
-figlet.getFonts()
+fonts = figlet.getFonts()
+f = ""
+if len sys.argv == 1:
+    f=random.choice(fonts)
+elif len sys.argv == 3:
 figlet.setFont(font=f)
 user_input = input("Input: ")
 print("Output:",figlet.renderText(s))
+
+
