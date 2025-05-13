d85e11543e5e884e63c19f3be564f3bfef5d363a
ThaiBenjamin <132616645+ThaiBenjamin@users.noreply.github.com>
2025-05-13 02:26:12 +0000

    Mon, May 12, 2025, 7:26 PM -07:00

--- watch/watch.py
+++ watch/watch.py
@@ -10,7 +10,7 @@ def parse(s):
     if re.search(r"<iframe(.)*><\/iframe>", s):
         match = re.search(r"https?://(?:www\.)?youtube\.com/embed/([a-z_A-Z_0-9]+)",s)
         if match:
-            return "https://you.tub/" + match.group(1)
+            return "https://youtu.be/" + match.group(1)
     else:
         return None
 
