5c3cba27f6c9c3d6bce9db9009625531cc61e75b
ThaiBenjamin <132616645+ThaiBenjamin@users.noreply.github.com>
2025-05-13 01:26:01 +0000

    Mon, May 12, 2025, 6:26 PM -07:00

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -1,13 +1,14 @@
 from numb3rs import validate
 
 
-def test_true():
+def test_format():
     assert validate(r"0.0.0.0") == True
-    assert validate(r"255.255.255.255") == True
-    assert validate(r"200.140.0.5") == True
+    assert validate(r"0.0.0") == False
+    assert validate(r"0.0") == False
+
 
 def test_false():
     assert validate(r"0.0.0") == False
     assert validate(r"275.10.0.0") == False
-    assert validate(r"5.0.200.250") == False
+    assert validate(r"5.0.200.256") == False
     assert validate(r"hi my name is ben") == False
