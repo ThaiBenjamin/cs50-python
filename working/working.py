50030232d9f2d78bb625fd0b7915b4bfea4e08c4
ThaiBenjamin <132616645+ThaiBenjamin@users.noreply.github.com>
2025-05-13 02:58:07 +0000

    Mon, May 12, 2025, 7:58 PM -07:00

--- working/working.py
+++ working/working.py
@@ -7,7 +7,19 @@ def main():
 
 
 def convert(s):
-    if re.search(r".+ (?:AMto .+",s):
+    if match := re.search(r"([0-9_: ]+(?:AM)?(?:PM)?) to ([0-9_: ]+(?:AM)?(?:PM)+)",s):
+        first_number = match.group(1).strip()
+        second_number = match.group(2).strip()
+
+        if "PM" in first_number:
+            first_number = first_number.remove("PM")
+            print(first_number)
+        if "PM" in second_number:
+            second_number = second_number.remove("AM")
+            print(second_number)
+
+    else:
+        raise ValueError
 
 
 if __name__ == "__main__":
