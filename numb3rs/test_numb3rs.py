6d723396181022f7798df069884db2c7bdb38f7a
ThaiBenjamin <132616645+ThaiBenjamin@users.noreply.github.com>
2025-05-13 00:52:09 +0000

    Mon, May 12, 2025, 5:52 PM -07:00

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -6,4 +6,7 @@ def test_true():
     assert validate("200.140.0.5") == True
 
 def test_false():
-    assert validate(")
+    assert validate("0.0.0") == False
+    assert validate("275.10.0.0") == False
+    assert validate("5.0.200.250") == False
+    assert validate("hi my name is ben") == False
