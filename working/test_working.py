68a92a2fd9d131e926d4f9e17ebc133c5831a44d
ThaiBenjamin <132616645+ThaiBenjamin@users.noreply.github.com>
2025-05-13 04:03:31 +0000

    Mon, May 12, 2025, 9:03 PM -07:00

--- working/test_working.py
+++ working/test_working.py
@@ -2,16 +2,17 @@ import pytest
 from working import convert
 
 
-def true_cases():
+def test_true_cases():
     assert convert(r"9 AM to 5 PM") == "09:00 to 17:00"
     assert convert(r"9:00 AM to 5:00 PM") == "09:00 to 17:00"
     assert convert(r"10 AM to 8:50 PM") == "10:00 to 20:50"
     assert convert(r"10:30 PM to 8 AM") == "22:30 to 08:00"
 
 
-def false_cases():
+def test_false_cases():
      with pytest.raises(ValueError):
         convert(r"9:60 AM to 5:60 PM")
         convert(r"9 AM - 5 PM")
         convert(r"09:00 AM - 17:00 PM")
+        convert(r"9:50 AM 5:40 PM")
 
