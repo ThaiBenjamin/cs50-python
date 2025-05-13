3a1dafe6db336c5e303f8fc7e1ee69b533538572
ThaiBenjamin <132616645+ThaiBenjamin@users.noreply.github.com>
2025-05-13 03:03:34 +0000

    Mon, May 12, 2025, 8:03 PM -07:00

--- working/working.py
+++ working/working.py
@@ -8,8 +8,8 @@ def main():
 
 def convert(s):
     if match := re.search(r"([0-9_: ]+(?:AM)?(?:PM)?) to ([0-9_: ]+(?:AM)?(?:PM)+)",s):
-        first_number_components = match.group(1).split(r"[: ]")
-        second_number_components = match.group(2).split(r"[: ]")
+        first_number_components = match.group(1).split(r":\s*")
+        second_number_components = match.group(2).split(r":\s*")
 
         print(first_number_components)
 
